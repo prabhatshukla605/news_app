5a74a4616dd9c010739afb994eacc815326bb897
prabhatshukla605 <prabhatshukla605@gmail.com>
2023-10-31 22:54:14 +0530

    changes made in news component

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const News = (props) => {
+  const date = props.data.date.getDate();
+  const day = props.data.date.toLocaleDateString("en-us", { weekday: "long" });
+
   console.log(props);
   return (
     <div id="container">
@@ -10,21 +13,21 @@ const News = (props) => {
         <div className="card__details">
           <div>
             {props.data.hashtag.map((item) => (
-              <span key={Math.random()} className="tag">{item}</span>
+              <span key={Math.random()} className="tag">
+                {item}
+              </span>
             ))}
             {/* <span className="tag">{props.data.hashtag}</span> */}
           </div>
 
-          <div className="name">
-            India Won World Cup 2023 against Pak by 6 wicket
-          </div>
+          <div className="name">{props.data.heading}</div>
           <p className="description">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, in
             accusantium. Inventore, voluptate velit. Asperiores dolore, quas
             labore delectus, laborum eligendi dolorum magni aspernatur deserunt
             aperiam illo fugiat est commodi?
           </p>
-          <p className="date">8:30 pm on Monday, 30 October, 2023</p>
+          <p className="date">{`8:30 pm on ${day}, ${date} October, 2023`}</p>
           <p className="source">Source: Google, Youtube</p>
         </div>
       </div>
