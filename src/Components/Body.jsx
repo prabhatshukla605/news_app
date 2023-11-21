c586ca3622e04a059a99d8cf49734c832425ee54
prabhatshukla605 <prabhatshukla605@gmail.com>
2023-11-21 22:20:35 +0530

    news card updated

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,32 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import NewsCard from "./NewsCard";
 
 const Body = () => {
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://www.inshorts.com/api/en/news?category=top_stories&max_limit=10&include_card_data=true"
+    );
+    const data = await res.json();
+    setNews(data.data.news_list);
+    setIsLoading(false);
+  };
+
   return (
     <div className="wrapper">
-      <div className="card">
-        <img
-          src="https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2023/11_nov/18_sat/img_1700307000567_837.jpg?"
-          alt="Image"
-        />
-        <div className="description">
-          <h2 className="headline">
-            Who have won Player of the Tournament award in ODI World Cup
-            history?
-          </h2>
-          <p className="date">09:15pm on sunday</p>
-          <p className="content">
-            The first-ever Player of the Tournament award in ODI World Cup
-            history was given in 1992 to New Zealand's Martin Crowe. SL's Sanath
-            Jayasuriya, SA's Lance Klusener, India's Sachin Tendulkar bagged it
-            in 1996, 1999 and 2003 editions, respectively. Other winners are
-            Australia's Glenn McGrath (2007), India's Yuvraj Singh (2011),
-            Australia's Mitchell Starc (2015) and NZ's Kane Williamson (2019).
-          </p>
-          <p className="source">
-            read more at <a target="_blank" href=""></a>
-          </p>
-        </div>
-      </div>
+      {isLoading ? <h1>Loading....</h1> : news.map((curr) => (
+        <NewsCard key={curr.hash_id} data={curr.news_obj} />
+      ))}
+      {/* {isLoading ? <h1>Loading....</h1> : <NewsCard data={news} />} */}
     </div>
   );
 };
--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const NewsCard = () => {
+const NewsCard = ({ data }) => {
+  const date = new Date(data.created_at);
+  const time = date.toLocaleString("en-IN", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  const day = date.toLocaleDateString("en-IN", { weekday: "long" });
+
   return (
-    <div>NewsCard</div>
-  )
-}
+    <div className="card">
+      <img src={data.image_url} alt="Image" />
+      <div className="description">
+        <h2 className="headline">{data.title}</h2>
+        <p className="date">
+          {time} on {day}
+        </p>
+        <p className="content">{data.content}</p>
+        <p className="source">
+          read more at
+          <a className="link" target="_blank" href={data.source_url}>
+            {" " + data.source_name}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default NewsCard
+export default NewsCard;
